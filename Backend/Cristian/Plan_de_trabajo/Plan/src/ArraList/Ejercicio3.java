5f1e8fe3781095cec98f03661a6ca884dbecc79b
GrandWeed777 <ll3925340@gmail.com>
2026-02-11 19:59:25 -0500

    Se actualizo el plan de estudio

--- Backend/Cristian/Plan_de_trabajo/Plan/src/ArraList/Ejercicio3.java
+++ Backend/Cristian/Plan_de_trabajo/Plan/src/ArraList/Ejercicio3.java
@@ -1,10 +1,35 @@
 package ArraList;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Double> precios = new ArrayList<Double>();
+		
+		precios.add(3.500);
+		precios.add(2.300);
+		precios.add(1.100);
+		precios.add(1.000);
+		precios.add(6.000);
+		
+		int indicePorModificar = 2;
+		double NuevoPrecio = 2.000;
+		
+		if (indicePorModificar >= 0  && indicePorModificar < precios.size()) {
+			precios.set(indicePorModificar, NuevoPrecio);
+			JOptionPane.showMessageDialog(null, "Precios en indice " + indicePorModificar + " Modificado a: " + NuevoPrecio);
+		}
+		
+		double totalCompra = 0;
+		for (Double c : precios) {
+			totalCompra += c;
+		}
+		
+		JOptionPane.showMessageDialog(null,"El total de la compra es " + totalCompra);
+		
 	}
 
 }
