f8d3bce89f1812372d9183c353391d40ed808653
Vera <ll3925340@gmail.com>
2026-05-11 15:07:29 -0500

    feat: implement load and clear logic with setTimeout

--- Frontend/Alejo/Angular/Entregables/directivas/src/app/app.ts
+++ Frontend/Alejo/Angular/Entregables/directivas/src/app/app.ts
@@ -13,6 +13,20 @@ type EstadoCarga = 'PENDIENTE' | 'CARGANDO' | 'EXITO';
 export class App {
   estadoActual: EstadoCarga = 'PENDIENTE';
   items: string[] = [];
+
+  manejarClick() {
+    if (this.estadoActual === 'EXITO') {
+        this.items = [];
+        this.estadoActual = 'PENDIENTE';
+    } else if (this.estadoActual === 'PENDIENTE') {
+        this.estadoActual = 'CARGANDO';
+
+        setTimeout(() => {
+          this.items = ['ONE','TWO','TREE'];
+          this.estadoActual = 'EXITO'
+        }, 2000);
+    }
+  }
 }
 
 
